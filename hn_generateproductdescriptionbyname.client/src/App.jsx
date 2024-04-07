e83aeccafdc1868e5f3f7dbc9d2468b3cf4108d5
Oleg <olegbelii4311@gmail.com>
2024-04-07 13:09:26 +0300

    новая надпись

--- hn_generateproductdescriptionbyname.client/src/App.jsx
+++ hn_generateproductdescriptionbyname.client/src/App.jsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import { Routes, Route, Link } from 'react-router-dom'; // Убираем BrowserRouter
-import { Container, AppBar, Toolbar, Typography, Button } from '@mui/material';
-import HomePage from './pages/HomePage.jsx';
-import NotFoundPage from './pages/NotFoundPage.jsx';
+import React from "react";
+import { Routes, Route } from "react-router-dom"; // Убираем BrowserRouter
+import { Container } from "@mui/material";
+import HomePage from "./pages/HomePage.jsx";
+import NotFoundPage from "./pages/NotFoundPage.jsx";
 
 function App() {
-    return (
-        <Container maxWidth="lg">
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="*" element={<NotFoundPage />} />
-            </Routes>
-        </Container>
-    );
+  return (
+    <Container>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </Container>
+  );
 }
 
 export default App;
